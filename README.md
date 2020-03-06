d843a2a4e860bdf3bfffd513fa05b6004bc357cb
bbbbbr <bbbbbr@users.noreply.github.com>
2020-03-05 16:15:18 -0800

    Initial commit, Makefile working, test build with static background image

---
+++ README.md
@@ -0,0 +1,9 @@
+Petris, a puzzle game for the Game Boy Color (CGB)
+===========
+
+
+
+Built with GBDK - http://gbdk.sourceforge.net/
+GBDK / GBTD
+GIMP Tilemap Helper
+GIMP Tilemap GB
---
+++ src/main.c
@@ -0,0 +1,49 @@
+#include <gb/gb.h>
+#include <gb/cgb.h> // Include cgb functions
+
+#include "../res/bg_map.h"
+#include "../res/bg_tiles.h"
+
+
+// These palettes replace the default palette with the appropriate colors. See cgb.h for more defined colors.
+
+UWORD bgPalette[] = {
+    bg_tilesCGBPal0c0, bg_tilesCGBPal0c1, bg_tilesCGBPal0c2, bg_tilesCGBPal0c3,
+    bg_tilesCGBPal1c0, bg_tilesCGBPal1c1, bg_tilesCGBPal1c2, bg_tilesCGBPal1c3,
+    bg_tilesCGBPal2c0, bg_tilesCGBPal2c1, bg_tilesCGBPal2c2, bg_tilesCGBPal2c3,
+    bg_tilesCGBPal3c0, bg_tilesCGBPal3c1, bg_tilesCGBPal3c2, bg_tilesCGBPal3c3,
+    bg_tilesCGBPal4c0, bg_tilesCGBPal4c1, bg_tilesCGBPal4c2, bg_tilesCGBPal4c3,
+    bg_tilesCGBPal5c0, bg_tilesCGBPal5c1, bg_tilesCGBPal5c2, bg_tilesCGBPal5c3,
+    bg_tilesCGBPal6c0, bg_tilesCGBPal6c1, bg_tilesCGBPal6c2, bg_tilesCGBPal6c3,
+    bg_tilesCGBPal7c0, bg_tilesCGBPal7c1, bg_tilesCGBPal7c2, bg_tilesCGBPal7c3,
+};
+
+void main(void){
+
+    // Check for CGB
+    // REG_A != 0x11
+//    if (HARDWARE_TYPE != 0x80U) {
+
+        // Handle Non-CGB
+//    }
+    //else
+    {
+        // Switch CGB fast mode
+        cpu_fast();
+        SPRITES_8x8;
+
+        set_bkg_palette(0, 8, bgPalette); // UBYTE first_palette, UBYTE nb_palettes, UWORD *rgb_data
+
+        VBK_REG = 1;
+        set_bkg_tiles(0, 0, 20, 18, bg_mapPLN1);
+
+        VBK_REG = 0;
+        set_bkg_tiles(0, 0, 20, 18, bg_mapPLN0);
+
+        set_bkg_data(0, 63, bg_tiles);
+
+        SHOW_BKG;
+
+        DISPLAY_ON;
+    }
+}
