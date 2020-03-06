453a42204e71a1268471ffd01f4ea9fe4ef9cd22
bbbbbr <bbbbbr@users.noreply.github.com>
2020-03-05 17:40:03 -0800

    Testing: press A/B to flip background/BG map attribute bits, Add interrupt and vblank init, add keypress handling

--- src/main.c
+++ src/main.c
@@ -4,6 +4,33 @@
 #include "../res/bg_map.h"
 #include "../res/bg_tiles.h"
 
+void init (void);
+void init_background (void);
+
+void init_interrupts(void);
+void vbl_update(void);
+
+
+void video_setflip(UINT8);
+
+
+UINT8 vbl_count;
+
+UINT8 keys;
+UINT8 previous_keys;
+#define UPDATE_KEYS() previous_keys = keys; keys = joypad()
+
+#define KEY_PRESSED(K) (keys & (K))
+#define KEY_TICKED(K)   ((keys & (K)) && !(previous_keys & (K)))
+#define KEY_RELEASED(K) ((previous_keys & (K)) && !(keys & (K)))
+
+#define ANY_KEY_PRESSED (keys)
+#define ANY_KEY_TICKED  (keys != previous_keys) // TODO: this is sloppy
+
+#define BG_PRIORITY 0x80
+#define BG_FLIP_Y   0x40
+#define BG_FLIP_X   0x20
+
 
 // These palettes replace the default palette with the appropriate colors. See cgb.h for more defined colors.
 
@@ -18,45 +45,84 @@ UWORD bgPalette[] = {
     bg_tilesCGBPal7c0, bg_tilesCGBPal7c1, bg_tilesCGBPal7c2, bg_tilesCGBPal7c3,
 };
 
-void main(void){
+void vbl_update() {
+    vbl_count ++;
+}
 
-    // Check for CGB
-    // REG_A != 0x11
-//    if (HARDWARE_TYPE != 0x80U) {
 
-        // Handle Non-CGB
-//    }
-    //else
-    {
-        // Switch CGB fast mode
-        cpu_fast();
-        SPRITES_8x8;
+void init_interrupts() {
+    disable_interrupts();
+    add_VBL(vbl_update);
+    set_interrupts(VBL_IFLAG);
+    enable_interrupts();
+}
+
+void init (void) {
+    // Switch CGB fast mode
+    cpu_fast();
+    SPRITES_8x8;
+
+    init_background();
+}
+
+void init_background (void) {
 
         set_bkg_palette(0, 8, bgPalette); // UBYTE first_palette, UBYTE nb_palettes, UWORD *rgb_data
         set_bkg_data(0, 63, bg_tiles);
 
-//        // Load BG tile attribute map
-//        VBK_REG = 1;
-//        set_bkg_tiles(0, 0, 20, 18, bg_mapPLN1);
-//
-//        // Load BG tile map
-//        VBK_REG = 0;
-//        set_bkg_tiles(0, 0, 20, 18, bg_mapPLN0);
-//
-//        SHOW_BKG;
-
         // Load BG tile attribute map
         VBK_REG = 1;
-        set_win_tiles(0, 0, 20, 18, bg_mapPLN1);
+        set_bkg_tiles(0, 0, 20, 18, bg_mapPLN1);
 
         // Load BG tile map
         VBK_REG = 0;
-        set_win_tiles(0, 0, 20, 18, bg_mapPLN0);
+        set_bkg_tiles(0, 0, 20, 18, bg_mapPLN0);
 
-        WX_REG = 7;
-        WY_REG = 0;
-        SHOW_WIN;
+        SHOW_BKG;
 
         DISPLAY_ON;
+}
+
+void video_setflip(UINT8 bg_attrib) {
+
+    #define VBUF_SIZE 18 * 20
+    int x;
+    UINT8 video_buff[ VBUF_SIZE ];
+
+    VBK_REG = 1; // Select BG tile attribute map
+
+        get_bkg_tiles(0, 0, 20, 18, &video_buff[0]);
+
+        for (x=0; x < VBUF_SIZE; x++) {
+            video_buff[x] ^= bg_attrib;
+        }
+
+        set_bkg_tiles(0, 0, 20, 18, &video_buff[0]);
+
+    VBK_REG = 0; // Re-Select regular BG tile map
+}
+
+void main(void){
+    init();
+
+    while(1) {
+        // Wait for vertical blank
+        // (skip if already happened)
+        if(!vbl_count)
+            wait_vbl_done();
+        vbl_count = 0;
+
+        UPDATE_KEYS();
+
+        // if (ANY_KEY_TICKED) {
+        //     video_setflip(keys);
+        // }
+        if (KEY_TICKED(J_A)) {
+            video_setflip(BG_FLIP_X);
+        }
+        else if (KEY_TICKED(J_B)) {
+            video_setflip(BG_FLIP_Y);
+        }
+
     }
 }
