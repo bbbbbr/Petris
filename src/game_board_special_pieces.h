0aa61e6133133d8ceaa8fe19e4ec33f5460275c0
bbbbbr <bbbbbr@users.noreply.github.com>
2020-03-26 16:36:44 -0700

    Special Pieces: adjust lightening piece to every 64 pieces

--- src/game_board_special_pieces.h
+++ src/game_board_special_pieces.h
@@ -6,7 +6,7 @@
 // TODO: load special piece handling into a LUT with Difficulty Level options
 #define PIECE_SPECIAL_THRESHOLD_BOMB 5 // TODO: create real criteria for giving the user a special piece
 
-#define PIECE_SPECIAL_THRESHOLD_LIGHTENING 0x0003 // TODO: create real criteria for giving the user a special piece
+#define PIECE_SPECIAL_THRESHOLD_LIGHTENING 0x003F // once every 64 // TODO: create real criteria for giving the user a special piece
 
 void board_handle_special_bomb(UINT8,UINT8);
 
