ad1a25bcba732c92f28fcf9995849648005c109a
bbbbbr <bbbbbr@users.noreply.github.com>
2020-05-03 16:10:28 -0700

    Gfx: Add tiles for Level up screen clear bar

--- res/intro_screen_tiles.h
+++ res/intro_screen_tiles.h
@@ -10,7 +10,7 @@
   Compression          : None.
   Counter              : None.
   Tile size            : 8 x 8
-  Tiles                : 0 to 67
+  Tiles                : 0 to 68
 
   Palette colors       : Included.
   SGB Palette          : None.
