e584fd3c0b5e68e7406467a2063b924622fcead0
bbbbbr <bbbbbr@users.noreply.github.com>
2020-05-05 17:42:37 -0700

    Licensing: Add License file and header to source files

--- src/game_piece_data.c
+++ src/game_piece_data.c
@@ -1,3 +1,14 @@
+// Copyright 2020 (c) bbbbbr
+//
+// This software is licensed under:
+//
+// For the purposes of this project "Share Alike" shall also include
+// distribution of the source code and any changes to it.
+//
+// CC-BY-NC-SA: Attribution Non-Commercial Share Alike Creative Commons
+// Attribution-NonCommercial-ShareAlike 4.0 International License
+// See: http://creativecommons.org/licenses/by-nc-sa/4.0/
+
 // game_piece_data.c
 
 #include <gb/gb.h>
