21d45c62a5a0f27264b1ecec747fadc420128e69
bbbbbr <bbbbbr@users.noreply.github.com>
2020-07-15 22:24:23 -0700

    GFX: Palette: Fade: Reduced flicker during palette fading. * Use a large array in RAM to buffer the entire faded palette, then write them all at once (instead of writing each of the 8 bg & sprite palettes one at a time) * Sync the writes to start once in VBlank to further reduce flicker * Add a switch to select between WHITE/BLACK/NOFADE * Slow down fade speed a little

--- src/fade.c
+++ src/fade.c
@@ -4,6 +4,10 @@
 #include "fade.h"
 
 
+#define PAL_FADE_WHITE
+// #define PAL_FADE_BLACK
+// #define PAL_FADE_NONE
+
 #define FADE_IN_START 5
 #define FADE_OUT_START 0
                         //   x    R    G    B
@@ -19,19 +23,24 @@ const UWORD FADE_LUT[6] = {0b0000000000000000,  // No white present to mix
 const UWORD * p_FadePalBg[8]     = {FADE_LUT, FADE_LUT, FADE_LUT, FADE_LUT, FADE_LUT, FADE_LUT, FADE_LUT, FADE_LUT};
 const UWORD * p_FadePalSprite[8] = {FADE_LUT, FADE_LUT, FADE_LUT, FADE_LUT, FADE_LUT, FADE_LUT, FADE_LUT, FADE_LUT};
 
-#define NUM_COLORS_PER_PAL 4 // (sizeof(UWORD) * 4)
+#define NUM_COLORS_PER_PAL 4
 
+UWORD ModPalBg[32];
+UWORD ModPalSprite[32];
 
 void fade_set_pal(UINT8 start_pal, UINT8 num_pal, UWORD * p_SrcPal, UINT8 which_pal) {
     int c;
 
-    // // Pass palette update through to GBDK pal function
+    // **DO NOT** Pass palette update through to GBDK pal function
+    // Instead let fade_start() perform all palette updates
+    //
     // if (which_pal == FADE_PAL_BKG)
     //     set_bkg_palette(start_pal, num_pal, p_SrcPal);
     // else
     //     set_sprite_palette(start_pal, num_pal, p_SrcPal);
 
-    // Now copy palette pointers into fade palette array slots
+    // Copy palette pointers into fade palette array slots
+    // for later reference
     for (c = start_pal; c < (start_pal + num_pal); c++) {
 
         if (which_pal == FADE_PAL_BKG)
@@ -48,17 +57,19 @@ void fade_start(INT8 fade_dir) {
 
     INT8 i;
     UINT8 pal, c;
-    UWORD palette_bg[4]; // Background Pal
-    UWORD palette_sp[4]; // Sprite Pal
     const UWORD * p_bg;
     const UWORD * p_sp;
+    UWORD * p_mod_bg;
+    UWORD * p_mod_sp;
+    UWORD fade_mask;
 
     i = (fade_dir == FADE_OUT) ? FADE_OUT_START : FADE_IN_START; // Set loop start (5 for fade in, 0 for fade out)
 
     while ((i != -1) && (i != 6)) {
 
-        // TODO: There is some flickering since palette rewrite timing is not locked to Vblank (and seems to take too long to compelte)
-        // while((STAT_REG & 0x03) != 0x01);
+        p_mod_bg = &ModPalBg[0];
+        p_mod_sp = &ModPalSprite[0];
+
         for(pal = 0; pal < 8; pal++) {
             // Load pointer to palette from cached values
             p_bg = p_FadePalBg[pal];
@@ -66,22 +77,53 @@ void fade_start(INT8 fade_dir) {
 
             // Read colors from palette and get faded version
             for(c = 0; c < 4; ++c) {
+
+                #ifdef PAL_FADE_WHITE
                     // Fade in/out from White
-                    palette_bg[c] = (*p_bg | FADE_LUT[i]);
-                    palette_sp[c] = (*p_sp | FADE_LUT[i]);
+                    fade_mask = FADE_LUT[i];
+                    *p_mod_bg = *p_bg | fade_mask;
+                    *p_mod_sp = *p_sp | fade_mask;
 
-                    // // Fade in/out from Black (if enabled, remove: = {FADE_LUT)
-                    // palette_bg[c] = ((*p_bg) >> i) & FADE_LUT[5 - i];
-                    // palette_sp[c] = ((*p_sp) >> i) & FADE_LUT[5 - i];
+                #else
+                  #ifdef PAL_FADE_BLACK
+                    // Fade in/out from Black
+                    fade_mask = FADE_LUT[5 - i];
+                    *p_mod_bg = ((*p_bg) >> i) & fade_mask;
+                    *p_mod_sp = ((*p_sp) >> i) & fade_mask;
 
-                    p_bg++;
-                    p_sp++;
+                    #else
+                    // No Fade
+                    *p_mod_bg = *p_bg;
+                    *p_mod_sp = *p_sp;
+                  #endif
+                #endif
+
+                // Move to next source palette color
+                p_bg++;
+                p_sp++;
+
+                // Move to next color in cached Modified palette
+                p_mod_bg++;
+                p_mod_sp++;
             };
-            // Apply current step of faded palette colors
-            set_bkg_palette(pal, 1, palette_bg);
-            set_sprite_palette(pal, 1, palette_sp);
         }
-        delay(10);
+        // Apply current step of faded palette colors
+
+        // To avoid flickering:
+        // Wait until VBlank (mode == 1) and then write palette in one big chunk
+        while((STAT_REG & 0x03) != 0x01);
+        set_bkg_palette(0, 8, ModPalBg);
+
+        while((STAT_REG & 0x03) != 0x01);
+        set_sprite_palette(0, 8, ModPalSprite);
+
+        // Wait a little while before performing the next step
+        #ifdef CPU_FAST_ENABLED
+            delay(70);
+        #else
+            delay(35);
+        #endif
+
         i += fade_dir;
     }
 }
