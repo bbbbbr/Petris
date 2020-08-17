d6f867c7d5135f7025b501d0f9184c20bf942c68
bbbbbr <bbbbbr@users.noreply.github.com>
2020-08-17 12:54:26 -0700

    Input: Waitpad_lowcpu: clarify vars and comments

--- src/input.c
+++ src/input.c
@@ -12,17 +12,23 @@ UINT8 key_repeat_count = 0x00;
 // Reduce CPU usage by only checking once per frame
 void waitpad_lowcpu(UINT8 button_mask, UINT8 wait_type) {
 
-    UINT8 test_result;
+    UINT8 any_buttons_pressed;
 
     // J_WAIT_ANY_PRESSED  waits until ANY masked button is pressed
     // J_WAIT_ALL_RELEASED waits until ALL masked buttons are released
     do {
         wait_vbl_done();
 
-        test_result = (joypad() & button_mask);
+        any_buttons_pressed = (joypad() & button_mask);
 
+        // If waiting for ANY PRESSED button then
+        // invert the test result so the loop exits
+        // if a single one is pressed
+        //
+        // Otherwise the loop will exit when there are
+        // no more buttons pressed (ALL RELEASED)
         if (wait_type == J_WAIT_ANY_PRESSED)
-            test_result = !test_result;
+            any_buttons_pressed = !any_buttons_pressed;
 
-    }  while (test_result);
+    }  while (any_buttons_pressed);
 }
