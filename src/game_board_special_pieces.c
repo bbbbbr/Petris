bbe4636692ee654f0cb5be921db7b089203aaea4
bbbbbr <bbbbbr@users.noreply.github.com>
2020-08-22 19:08:43 -0700

    Cleanup: FIXME removal

--- src/game_board_special_pieces.c
+++ src/game_board_special_pieces.c
@@ -35,26 +35,18 @@ void board_handle_special_bomb(INT8 x,INT8 y) {
     UINT8 c;
     UINT8 tx, ty;
 
-    // TODO: visual effects and sound effects on the tiles
-    //       maybe expand: board_clear_tile_xy() to set arbitray pieces and use it to flash the area
-    //
-    // Clear adjacent 4 tiles
-
-    // TODO: OPTIMIZE: make a loop?
-    // TODO: move the effects into board_clear_tile_xy?
-
-    // Remove hinting for any pets BOMB_HINT_OFFSET_LUT_X will get shortened by bomb
+    // Clear adjacent 4 tiles (no piece clearing player credit from this)
     c = 0;
-    // TODO: FIXME: Compiler problems with the for loop: Multiple definition of C$game_board_special_pieces.c$47�G��.�gO��@
-    // for (c = 0; c < ARRAY_LEN(BRD_BOMB_HINT_OFFSET_LUT_X); c++) {
+    // This isn't a for-loop because the compiler had some transient problem with that
     while (c < ARRAY_LEN(BRD_BOMB_HINT_OFFSET_LUT_X)) {
 
+        // Retrieve piece at adjacent location to bomb
         tx = x + BRD_BOMB_HINT_OFFSET_LUT_X[c];
         ty = y + BRD_BOMB_HINT_OFFSET_LUT_Y[c];
-
-        // Retrieve piece at adjacent location to bomb
-        // and clear it of any special piece hinting
         board_piece_get_xy(tx, ty, &piece, &connect);
+
+        // Clear any pet-length hinting from it since
+        // removal of piece would alter length
         board_check_completed_pet_xy(tx, ty, piece, connect,
                                      BRD_CHECK_FLAGS_DONT_ADD_POINTS
                                      | BRD_CHECK_FLAGS_DONT_HANDLE_PET_COMPLETED);
@@ -65,16 +57,4 @@ void board_handle_special_bomb(INT8 x,INT8 y) {
 
         c++;
     }
-
-    // PLAY_SOUND_TILE_CLEAR_BOMB;
-    // board_clear_tile_xy(x - 1, y);
-
-    // PLAY_SOUND_TILE_CLEAR_BOMB;
-    // board_clear_tile_xy(x + 1, y);
-
-    // PLAY_SOUND_TILE_CLEAR_BOMB;
-    // board_clear_tile_xy(x, y - 1);
-
-    // PLAY_SOUND_TILE_CLEAR_BOMB;
-    // board_clear_tile_xy(x, y + 1);
 }
