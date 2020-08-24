bbbbbr/Petris

commit 529711be8d1a90ada5c0decdb2d1a234cb561eb9

Game Piece: Merge new piece counting (and triggering of special pieces) into new piece generation code. A little tidy up

src/game_piece.c
@@ -19,6 +19,7 @@
 #include "serial_link.h"
 
 #include "options.h"
+#include "gfx.h"
 #include "game_piece.h"
 #include "game_piece_data.h"
 #include "player_info.h"
@@ -58,32 +59,39 @@ void game_piece_next_generate(void) {
         // Otherwise generate a single random new pet tile piece
 
         // If connected by link then only use rand() so that the
-        // games start with the same random number sequence
+        // games follow the same random number sequence
         if (link_status == LINK_STATUS_CONNECTED) {
+            // 2-Player mode
             game_piece_next = ((UINT8)rand() & 0x1F);
         } else {
-            // In 1-player mode
-            // Use rand() ^ DIV_REG to add more variety to the random number sequence
-            // game_piece_next = ((UINT8)DIV_REG & 0x1F);
+            // In 1-player mode mix in DIV_REG for a little more variety to the random number sequence
             game_piece_next = ((UINT8)(rand() ^ DIV_REG) & 0x1F);
         }
 
-        // In Tail Cleanup mode
-        // suppress tail pieces to make it less frustrating
+        // Increase the count of total pieces used in the game
+        // if (player_numpieces < 0xFFFE) // Probably not necessary to prevent wraparound
+        player_numpieces++;
+
+        // Check to see whether a special piece (merge) should be delivered
+        if ((player_numpieces & p_game_settings->spec_merge_threshold_pieces) == p_game_settings->spec_merge_threshold_pieces)
+            game_piece_next_set(GP_SPECIAL_LIGHTENING);
+
+
+        // == New piece adjusments to improve player experience in various modes ==
+
+        // Tail Cleanup : suppress adding tail pieces to make it less frustrating
         if ((option_game_type == OPTION_GAME_TYPE_PET_CLEANUP) &&
             ((game_piece_next & GP_SEG_MASK) == GP_SEG_TAIL_BITS)) {
 
             // Translate Tail pieces to Head pieces
             game_piece_next = (game_piece_next & ~GP_SEG_MASK) | GP_SEG_HEAD_BITS;
 
-        // In Crunch-up mode
-        // suppress all L-Turn pieces to make it less frustrating
+        // Crunch-up mode: suppress all L-Turn pieces to make it less frustrating
         } else if ((option_game_type == OPTION_GAME_TYPE_CRUNCH_UP) &&
             ((game_piece_next & GP_SEG_MASK) == GP_SEG_TURN_BITS)) {
 
             // Translate L-Turn pieces to Torso
             game_piece_next = (game_piece_next & ~GP_SEG_MASK) | GP_SEG_TORSO_BITS;
-
         }
     }
 }

src/player_info.c
@@ -41,16 +41,6 @@ UINT16 player_numpieces;
 
 UINT8 level_increment_enqueue;
 
-void new_piece_count_increment(void) {
-    player_numpieces++;
-
-    // Check to see whether a special piece (merge) should be delivered
-    if ((player_numpieces & p_game_settings->spec_merge_threshold_pieces) == p_game_settings->spec_merge_threshold_pieces)
-        game_piece_next_set(GP_SPECIAL_LIGHTENING);
-
-}
-
-// extern game_speed_frames_per_drop; // TODO: DEBUG: REMOVE
 
 
 void player_info_display(void) {

src/player_info.h
@@ -55,11 +55,11 @@
 // Transition to a new palette every N levels
 #define LEVEL_CHANGE_PAL_NUM 5 // (5 alternate palettes total)
 
-// TODO: move this to a more centralized options/settings area?
 // #define PLAYER_TILES_PER_LEVEL  20 // 25
 #define PLAYER_PETS_PER_LEVEL   20 // 10
 
 extern UINT16 player_level;
+extern UINT16 player_numpieces;
 extern UINT8 level_increment_enqueue;
 
 void score_update(UINT16 num_tiles);
@@ -72,8 +72,6 @@ void level_show(void);
 
 void player_info_display(void);
 void player_info_newgame_reset(void);
-
-void new_piece_count_increment(void);
 
 #endif // PLAYER_INFO_H
 

src/player_piece.c
@@ -80,13 +80,7 @@ UINT8 player_piece_try_reload(void) {
     player_piece = game_piece_next_get();
 
     // Generate the next upcoming piece and display it
-    // TODO: to simplify, move this into game_piece_next_get()?
     game_piece_next_generate();
-
-    // Count the new piece
-    // --> this should be called after game_piece_next_generate()
-    // TODO: move this into game_piece_next_generate?
-    new_piece_count_increment();
 
     // Show a preview of the next piece, if applicable
     game_piece_next_show(TRUE);

commit d3b6366d025df97671268d8c24d08825f55614ee

Bug Fix: - 4-way hinting sprites have the wrong sprite ("9" instead of "[ ]") in TAIL CLEANUP mode. - FIX: don't test for and translate special pieces (tail -> head) - Error was introduced in 529711be8d1a90ada5c0decdb2d1a234cb561eb9 by merging piece counting into new piece generation - previously  new_piece_count_increment() was called *AFTER* game_piece_next_generate() - but it got merged into the middle of game_piece_next_generate() before the game-mode type piece translation (tails -> heads), so the special piece was getting translated into garbage since translation wasn't gated on special pieces. FIX: move the special test to after piece translation, also gate translation to prevent special pieces.

src/game_piece.c
@@ -68,39 +68,51 @@ void game_piece_next_generate(void) {
             game_piece_next = ((UINT8)(rand() ^ DIV_REG) & 0x1F);
         }
 
-        // Increase the count of total pieces used in the game
-        //
-        // Note: could prevent wraparound here, but if the player
-        // ever did reach this lofty amount it is probably better
-        // to let it wrap so they can continue to receive
-        // special pieces based on continued changes to this var
-        // (They are bitmask based so should still work)
-        //
-        // if (player_numpieces < PLAYER_NUMPIECES_MAX)
-            player_numpieces++;
-
-        // Check to see whether a special piece (merge) should be delivered
-        if ((player_numpieces & p_game_settings->spec_merge_threshold_pieces) == p_game_settings->spec_merge_threshold_pieces)
-            game_piece_next_set(GP_SPECIAL_LIGHTENING);
-
 
         // == New piece adjusments to improve player experience in various modes ==
 
-        // Tail Cleanup : suppress adding tail pieces to make it less frustrating
-        if ((option_game_type == OPTION_GAME_TYPE_PET_CLEANUP) &&
-            ((game_piece_next & GP_SEG_MASK) == GP_SEG_TAIL_BITS)) {
+        // A SPECIAL piece should NEVER make it in here, but just in
+        // case, this will future proof it against unwitting changes.
+        // (SPECIAL pieces would get mangled to invalid values)
+        if (!(game_piece_next & GP_SPECIAL_MASK)) {
 
-            // Translate Tail pieces to Head pieces
-            game_piece_next = (game_piece_next & ~GP_SEG_MASK) | GP_SEG_HEAD_BITS;
+            // Tail Cleanup : suppress adding tail pieces to make it less frustrating
+            if ((option_game_type == OPTION_GAME_TYPE_PET_CLEANUP) &&
+                ((game_piece_next & GP_SEG_MASK) == GP_SEG_TAIL_BITS)) {
 
-        // Crunch-up mode: suppress all L-Turn pieces to make it less frustrating
-        } else if ((option_game_type == OPTION_GAME_TYPE_CRUNCH_UP) &&
-            ((game_piece_next & GP_SEG_MASK) == GP_SEG_TURN_BITS)) {
+                // Translate Tail pieces to Head pieces
+                game_piece_next = (game_piece_next & ~GP_SEG_MASK) | GP_SEG_HEAD_BITS;
 
-            // Translate L-Turn pieces to Torso
-            game_piece_next = (game_piece_next & ~GP_SEG_MASK) | GP_SEG_TORSO_BITS;
+            // Crunch-up mode: suppress all L-Turn pieces to make it less frustrating
+            } else if ((option_game_type == OPTION_GAME_TYPE_CRUNCH_UP) &&
+                ((game_piece_next & GP_SEG_MASK) == GP_SEG_TURN_BITS)) {
+
+                // Translate L-Turn pieces to Torso
+                game_piece_next = (game_piece_next & ~GP_SEG_MASK) | GP_SEG_TORSO_BITS;
+            }
         }
     }
+
+
+    // Done generating the new piece.
+    // Now count it and queue up a special piece if applicable
+    // WARNING: This needs to be done AFTER and OUTSIDE of any
+    //          of the game-type piece massaging that happens above
+
+    // Increase the count of total pieces used in the game
+    //
+    // Note: could prevent wraparound here, but if the player
+    // ever did reach this lofty amount it is probably better
+    // to let it wrap so they can continue to receive
+    // special pieces based on continued changes to this var
+    // (They are bitmask based so should still work)
+    //
+    // if (player_numpieces < PLAYER_NUMPIECES_MAX)
+        player_numpieces++;
+
+    // Check to see whether a special piece (merge) should be delivered
+    if ((player_numpieces & p_game_settings->spec_merge_threshold_pieces) == p_game_settings->spec_merge_threshold_pieces)
+        game_piece_next_set(GP_SPECIAL_LIGHTENING);
 }
 
 