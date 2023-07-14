a14ad8f477c0d6e783cdf9efda8064072f410962
bbbbbr <bbbbbr@users.noreply.github.com>
2023-07-14 16:54:15 -0700

    Change to standard method of managing dual random number sequences

--- src/swaprand.c
+++ src/swaprand.c
@@ -1,13 +1,17 @@
 
-#ifndef SWAPRAND_INCLUDE
-#define SWAPRAND_INCLUDE
-
 #include <gbdk/platform.h>
 #include <stdint.h>
+#include <rand.h>
+
+uint16_t rand_alt = 0u;
+uint16_t rand_temp;
 
 /** Swaps between primary and secondary random number state vars.
     Allows running two sequences in parallel
 */
-void swaprand(void);
-
-#endif
+void swaprand(void) {
+	rand_temp = __rand_seed;
+	__rand_seed = rand_alt;
+	rand_alt = rand_temp;
+}
+	
