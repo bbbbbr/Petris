eeb558a4062edd1972077825fd6987bd5a1d65bb
bbbbbr <bbbbbr@users.noreply.github.com>
2024-04-17 17:51:39 -0700

    Fix compat problems with new SDCC calling conventions, add OLDCALL and remove some asm has been part of GBDK for a long time

--- src/unapack.h
+++ src/unapack.h
@@ -1,6 +1,14 @@
 #ifndef __UNAPACK_H_INCLUDE
 #define __UNAPACK_H_INCLUDE
 
-void UNAPACK(const unsigned char * sour, unsigned char * dest);
+#ifndef OLDCALL
+  #if __SDCC_REVISION >= 12608
+    #define OLDCALL __sdcccall(0)
+  #else
+    #define OLDCALL
+  #endif
+#endif
+
+void UNAPACK(const unsigned char * sour, unsigned char * dest) OLDCALL;
 
 #endif
