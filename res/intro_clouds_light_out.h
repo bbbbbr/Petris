b21ac621c2c122d3bf9c971b29551eae437768a2
bbbbbr <bbbbbr@users.noreply.github.com>
2026-09-10 13:10:16 -0700

    Intro Screen: Better clouds, more density and scrolling coverage

--- res/intro_clouds_light_out.h
+++ res/intro_clouds_light_out.h
@@ -2,12 +2,12 @@
 #define _intro_clouds_light_out_H
 #include <stdint.h>
 
-#define intro_clouds_light_out_PX_WIDTH       256
+#define intro_clouds_light_out_PX_WIDTH       512
 #define intro_clouds_light_out_PX_HEIGHT      88
 #define intro_clouds_light_out_PALLETTE_COUNT 3
 
 extern const uint16_t intro_clouds_light_out_palette[3];
 
-extern const uint8_t intro_clouds_light_out[11264];
+extern const uint8_t intro_clouds_light_out[22528];
 
 #endif
